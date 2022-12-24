b9a0247a81cf394a51798c14666e76eac7261286
lyzlyslyc <62510112+lyzlyslyc@users.noreply.github.com>
2022-12-24 13:06:43 +0800

    Add files via upload

--- Limited Games Fetch Script.js
+++ Limited Games Fetch Script.js
@@ -2,6 +2,7 @@ let data = {};
 data.banned={};
 data.FOD={};
 data.limited={};
+data.cards={};
 
 //被Ban游戏数据来源 https://steam-tracker.com/apps/banned
 document.querySelectorAll("#delisted-apps > tbody > tr").forEach(row=>{
@@ -29,4 +30,11 @@ document.querySelectorAll("#table-apps > tbody > tr").forEach(row=>{
     let appid = row.dataset.appid;
     let name = row.querySelector("td:nth-child(3)").innerText;
     data[appid]=name;
+})
+
+//卡牌游戏数据来源 https://steamdb.info/search/?a=app&q=&type=1&category=29&all=1
+document.querySelectorAll("#table-sortable > tbody > tr").forEach(row=>{
+    let appid = row.dataset.appid;
+    let name = row.querySelector("td:nth-child(3)").innerText;
+    data.cards[appid]=name;
 })
